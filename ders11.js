8a9b1c4264b0e9e6b1143a0cef05bb6750779d65
gokayybaz <gokaybaz1000@gmail.com>
2022-02-13 15:03:58 +0300

    gokaybaz: obje metodları tamamlandı

---
+++ ders11.js
@@ -0,0 +1,28 @@
+// Obje Metodları
+
+// Hemen bir obje oluşturalım ;
+
+let user = {
+    name: "Gökay",
+    surName: "Baz",
+    age: 18,
+    maas: 1200,
+}
+
+// user objesindeki istediğimiz özelliği konsola yazdıralım.
+console.log(user.maas)
+console.log(user["surName"])
+
+// user objesindeki özelliğin değerini farklı değişkene atayalım.
+let userName = user.name + " " + user.surName
+console.log(userName)
+
+// Objeler içerisine fonksiyonlarda yazabiliriz.
+let userManager = {
+    name: "Benan",
+    addUser: function (){
+        // this keywordü ile objenin içerisindeki fonksiyon içerisinden objenin elemanlarına ulaşabiliriz.
+        return this.name+" "+"adlı kullanıcı sisteme eklendi !"
+    }
+}
+
